bc7b401f75ccad86878f7bc6de93a39d80e84cb3
MoisesMG <menagonzalezmoises@gmail.com>
2016-11-14 18:16:57 -0600

    clase interna BaseColums implementada

--- app/src/main/java/app/rojoyazul/com/acceso_a_datos/PeopleHelper.java
+++ app/src/main/java/app/rojoyazul/com/acceso_a_datos/PeopleHelper.java
@@ -6,6 +6,7 @@ package app.rojoyazul.com.acceso_a_datos;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
 
 /**
  * Created by Moises on 14/11/2016.
@@ -24,11 +25,18 @@ public class PeopleHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        //aqui se crea la base datos
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
-    }
-}
+    }//fin del metodo
+
+    //definicion de clase interna
+    private class Contracto implements BaseColumns{
+        //aqui se definen las constantes para las columnas. La case BaseColumns ya incluye una interface
+        //con el campo id
+        static final String
+    }//fin de la clase interna
+}//fin de la clase
