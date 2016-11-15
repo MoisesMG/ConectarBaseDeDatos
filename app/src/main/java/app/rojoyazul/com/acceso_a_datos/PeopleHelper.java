ee2a270b35646bca086ea2acaaed2cc0f44b5af8
MoisesMG <menagonzalezmoises@gmail.com>
2016-11-14 18:30:35 -0600

    contantes staticas para los tipos de datos declaradas en la clase PeopleHelper

--- app/src/main/java/app/rojoyazul/com/acceso_a_datos/PeopleHelper.java
+++ app/src/main/java/app/rojoyazul/com/acceso_a_datos/PeopleHelper.java
@@ -16,6 +16,15 @@ public class PeopleHelper extends SQLiteOpenHelper {
     private static final String DATA_BASE_NAME = "people.db";
     private static final int DATA_BASE_VERSION = 1;
 
+    //esta es la sentencia para crear la tabla Persona
+    private static final String SQL_CREATE_TABLE = "CREATE TABLE "+ Contrato.TABLE_NAME +
+            "(" + Contrato._ID + "";
+
+    //definicion de los tipos de datos de las tablas
+    private static final String TEXT_TIPE= "TEXT";
+    private static final String INTEGER_TIPE= "INTEGER";
+
+
     /*el contexto es el lugar donde se almacena la aplicacion con colores, estilos, etc para que la base de datos
     * sepa a que aplicaicon pertenece, SqliteOpenHelper ya tiene un cursor por defecto por eso se pase null en el
     * contructor*/
@@ -26,7 +35,7 @@ public class PeopleHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         //aqui se crea la base datos
-    }
+    }//fin del metodo
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
@@ -34,9 +43,11 @@ public class PeopleHelper extends SQLiteOpenHelper {
     }//fin del metodo
 
     //definicion de clase interna
-    private class Contracto implements BaseColumns{
+    private class Contrato implements BaseColumns{
         //aqui se definen las constantes para las columnas. La case BaseColumns ya incluye una interface
         //con el campo id
-        static final String
+        static final String TABLE_NAME = "Persona";
+        static final String NOMBRE = "nombre";
+        static final String EDAD = "edad";
     }//fin de la clase interna
 }//fin de la clase
